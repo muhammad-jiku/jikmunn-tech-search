cf0892452ef568e7ff99b1b8113c592a1e397099
muhammad-jiku <muhammadjiku364@gmail.com>
2022-03-02 22:21:38 +0600

    final commit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-const searchMobile = () => {
-  const searchInput = document.getElementById('search');
-  const searchInputText = searchInput.value;
-  if (searchInputText == '') {
-    return messageSection(
+// device invoking function declared from html file
+const searchTechDevice = () => {
+  const searchTechDevice = document.getElementById('search');
+  const searchTechDeviceText = searchTechDevice.value.toLowerCase();
+  if (searchTechDeviceText == '') {
+    return messageToClientSection(
       'flex',
-      'Sorry! to bother you dear. Please, try to search tech device!'
+      `<h1>Sorry! to bother you dear. Please, try to search tech device!</h1>`
     );
   }
-  searchInput.value = '';
-  loadMobiles(searchInputText);
+  searchTechDevice.value = '';
+  loadDevices(searchTechDeviceText);
   toggleSpinner('flex', 'center');
-  messageSection('none', '');
+  messageToClientSection('none', '');
   toggleSingleSearchResult('none');
 };
 
@@ -21,7 +22,7 @@ const toggleSpinner = (displayStyle, displayStyleAlign) => {
   spinner.style.alignItems = displayStyleAlign;
 };
 
-const messageSection = (message, messgeText) => {
+const messageToClientSection = (message, messgeText) => {
   const textMessage = document.getElementById('message-section');
   textMessage.classList.add(
     'message-section-heading',
@@ -42,46 +43,38 @@ const toggleSingleSearchResult = (displaySingleResult) => {
 //   document.getElementById('see-more').style.display = displaySeeMore;
 // };
 
-const loadMobiles = async (searchInputText) => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchInputText}`;
+const loadDevices = async (searchDevice) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchDevice}`;
   try {
     const res = await fetch(url);
-    const searchMobileData = await res.json();
-    // if (Object.keys(searchMobileData).length <= 20) {
-    // console.log(Object.keys(searchMobileData?.data).length);
-    displaySearchResult(searchMobileData?.data.slice(0, 20));
-    // } else {
-    //   displaySearchResult(searchMobileData?.data.slice(20));
-    //   toggleSeeMore('block');
-    // }
+    const searchDeviceData = await res.json();
+    displaySearchResult(searchDeviceData?.data.slice(0, 20));
   } catch (error) {
     console.log(error.message);
   }
 };
 
-const displaySearchResult = (mobiles) => {
+const displaySearchResult = (techDevices) => {
   const parentSection = document.getElementById('search-result-card');
-  // parentSection.
   parentSection.textContent = '';
-  if (mobiles.length === 0) {
+  if (techDevices.length === 0) {
     return (
-      messageSection(
+      messageToClientSection(
         'flex',
-        'Sorry! your search device is not available right now!'
+        `<h1>Sorry! your search device is not available right now!</h1>`
       ) || toggleSpinner('none')
     );
   } else {
-    mobiles.forEach((mobile) => {
+    techDevices.forEach((device) => {
       const childSection = document.createElement('section');
       childSection.classList.add('col-12', 'col-lg-4');
-      console.log(mobile.length);
       childSection.innerHTML = `
         <section class="card h-100">
-            <img class="card-img-top img-fluid" src="${mobile?.image}" alt="pic">
+            <img class="card-img-top img-fluid" src="${device?.image}" alt="pic">
             <section class="card-body card-body-text">
-                <h2 class="card-title">${mobile?.brand}</h2>
-                <h4 class="card-text">${mobile?.phone_name}</h4>
-                <button onclick="phoneDetails('${mobile?.slug}')" class="btn btn-primary">Details</button>
+                <h2 class="card-title">${device?.brand}</h2>
+                <h4 class="card-text text-muted">${device?.phone_name}</h4>
+                <button onclick="techDeviceDetails('${device?.slug}')" class="btn btn-primary">Details</button>
             </section>
         </section>    
         `;
@@ -91,99 +84,101 @@ const displaySearchResult = (mobiles) => {
   toggleSpinner('none', '');
 };
 
-const phoneDetails = async (phoneId) => {
-  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+const techDeviceDetails = async (techDeviceId) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${techDeviceId}`;
   try {
     const res = await fetch(url);
-    const phoneDetailsData = await res.json();
-    displayPhoneDetails(phoneDetailsData?.data);
+    const techDeviceDetailsData = await res.json();
+    displayTechDeviceDetails(techDeviceDetailsData?.data);
   } catch (error) {
     console.log(error.message);
   }
 };
 
-const displayPhoneDetails = (details) => {
+const displayTechDeviceDetails = (techDeviceDetails) => {
   const parentSection = document.getElementById('single-result-card');
   parentSection.textContent = '';
   const childSection = document.createElement('section');
   childSection.innerHTML = `
       <section class="card">
-            <img class="img-fluid h-25 w-50 ms-5" src="${details?.image}">
+            <img class="img-fluid h-25 w-50 ms-5" src="${
+              techDeviceDetails?.image
+            }">
             <section class="card-header card-heading-text">
-                <h2 class="card-title">${details?.brand}</h2>
-                <h4 class="card-subtitle mb-2">${details?.name}</h4>
+                <h2 class="card-title">${techDeviceDetails?.brand}</h2>
+                <h4 class="card-subtitle mb-2">${techDeviceDetails?.name}</h4>
                 <h5 class="card-subtitle mb-2 text-muted">${
-                  details?.releaseDate
-                    ? details?.releaseDate
-                    : 'Release date unknown'
+                  techDeviceDetails?.releaseDate
+                    ? techDeviceDetails?.releaseDate
+                    : 'Release date information is not available at the moment'
                 }</h5>
             </section>
             <section class="card-body card-body-text">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${
-                      details?.mainFeatures?.chipSet
-                        ? details?.mainFeatures?.chipSet
-                        : 'ji'
+                    <li class="list-group-item"><span class="fw-bolder">Chip:</span> ${
+                      techDeviceDetails?.mainFeatures?.chipSet
+                        ? techDeviceDetails?.mainFeatures?.chipSet
+                        : 'Information is not available at the moment'
                     }</li>
-                    <li class="list-group-item">${
-                      details?.mainFeatures?.displaySize
-                        ? details?.mainFeatures?.displaySize
-                        : 'ji'
+                    <li class="list-group-item"><span class="fw-bolder">Display:</span> ${
+                      techDeviceDetails?.mainFeatures?.displaySize
+                        ? techDeviceDetails?.mainFeatures?.displaySize
+                        : 'Information is not available at the moment'
                     }</li>
-                    <li class="list-group-item">${
-                      details?.mainFeatures?.memory
-                        ? details?.mainFeatures?.memory
-                        : 'ji'
+                    <li class="list-group-item"><span class="fw-bolder">Memory:</span> ${
+                      techDeviceDetails?.mainFeatures?.memory
+                        ? techDeviceDetails?.mainFeatures?.memory
+                        : 'Information is not available at the moment'
                     }</li>
-                    <li class="list-group-item">${
-                      details?.mainFeatures?.storage
-                        ? details?.mainFeatures?.storage
-                        : 'ji'
+                    <li class="list-group-item"><span class="fw-bolder">Storage:</span> ${
+                      techDeviceDetails?.mainFeatures?.storage
+                        ? techDeviceDetails?.mainFeatures?.storage
+                        : 'Information is not available at the moment'
                     }</li>
-                    <li class="list-group-item">${
-                      details?.mainFeatures?.sensors
-                        ? details?.mainFeatures?.sensors
-                        : 'Iformation not found'
+                    <li class="list-group-item"><span class="fw-bolder">Sensors:</span> ${
+                      techDeviceDetails?.mainFeatures?.sensors
+                        ? techDeviceDetails?.mainFeatures?.sensors
+                        : 'Information is not available at the moment'
                     }</li>
-                    <li class="list-group-item">
+                    <li class="list-group-item"><span class="fw-bolder">Bluetooth:</span> 
                     ${
-                      details?.others?.Bluetooth
-                        ? details?.others?.Bluetooth
-                        : 'Iformation not found'
+                      techDeviceDetails?.others?.Bluetooth
+                        ? techDeviceDetails?.others?.Bluetooth
+                        : 'Information is not available at the moment'
                     }
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item"><span class="fw-bolder">USB:</span> 
                         ${
-                          details?.others?.USB
-                            ? details?.others?.USB
-                            : 'Iformation not found'
+                          techDeviceDetails?.others?.USB
+                            ? techDeviceDetails?.others?.USB
+                            : 'Information is not available at the moment'
                         }
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item"><span class="fw-bolder">GPS:</span> 
                     ${
-                      details?.others?.GPS
-                        ? details?.others?.GPS
-                        : 'Iformation not found'
+                      techDeviceDetails?.others?.GPS
+                        ? techDeviceDetails?.others?.GPS
+                        : 'Information is not available at the moment'
                     }
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item"><span class="fw-bolder">NFC:</span> 
                     ${
-                      details?.others?.NFC
-                        ? details?.others?.NFC
-                        : 'Iformation not found'
+                      techDeviceDetails?.others?.NFC
+                        ? techDeviceDetails?.others?.NFC
+                        : 'Information is not available at the moment'
                     }
                     </li>  
-                    <li class="list-group-item">
+                    <li class="list-group-item"><span class="fw-bolder">Radio:</span> 
                     ${
-                      details?.others?.Radio
-                        ? details?.others?.Radio
-                        : 'Iformation not found'
+                      techDeviceDetails?.others?.Radio
+                        ? techDeviceDetails?.others?.Radio
+                        : 'Information is not available at the moment'
                     }
                     </li>
-                    <li class="list-group-item">${
-                      details?.others?.WLAN
-                        ? details?.others?.WLAN
-                        : 'Iformation not found'
+                    <li class="list-group-item"><span class="fw-bolder">WLAN:</span> ${
+                      techDeviceDetails?.others?.WLAN
+                        ? techDeviceDetails?.others?.WLAN
+                        : 'Information is not available at the moment'
                     }
                     </li>
 
